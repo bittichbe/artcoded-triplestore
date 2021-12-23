06e8347ff84186cf0dbeb2a8e8d071600f5565e8
Nordine Bittich <nordine@bittich.be>
2021-12-23 16:45:55 +0100

    migrations capabilities

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # artcoded-triplestore
+
+e.g using security:
+```
+  triplestore:
+    image: nbittich/triplestore
+    restart: always
+    environment:
+      DATA_DIR: /triplestore
+      SECURITY_CONFIG: "classpath:config/security.yml"
+      ARTEMIS_CONFIG: "classpath:config/artemis.yml"
+      ARTEMIS_URL: tcp://artemis:61616
+      ARTEMIS_USER: root
+      ARTEMIS_PASSWORD: root
+      MIGRATION_PATH: /migrations
+      MIGRATION_DEFAULT_GRAPH: "https://bittich.be/application"
+      SERVER_PORT: 80
+      JWK_SET_URI: http://auth.somehost.org:8080/auth/realms/Artcoded/protocol/openid-connect/certs
+    volumes:
+      - ./data/tdb2:/triplestore
+      - ./data/migrations:/migrations
+    ports:
+      - 8888:80
+```
--- src/main/java/tech/artcoded/triplestore/tdb/TDBService.java
+++ src/main/java/tech/artcoded/triplestore/tdb/TDBService.java
@@ -1,20 +1,27 @@
 package tech.artcoded.triplestore.tdb;
 
+import com.google.common.collect.Lists;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Triple;
 import org.apache.jena.query.Dataset;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.update.UpdateRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tech.artcoded.triplestore.sparql.ModelUtils;
 import tech.artcoded.triplestore.sparql.QueryParserUtil;
 import tech.artcoded.triplestore.sparql.SparqlResult;
+
+import java.util.List;
 
 import static tech.artcoded.triplestore.sparql.QueryParserUtil.parseQuery;
 
@@ -22,6 +29,11 @@ import static tech.artcoded.triplestore.sparql.QueryParserUtil.parseQuery;
 @Slf4j
 public class TDBService {
   private final Dataset ds;
+
+  @Value("${triplestore.batchSize}")
+  private int batchSize;
+  @Value("${triplestore.maxRetry}")
+  private int maxRetry;
 
   public TDBService(Dataset ds) {
     this.ds = ds;
@@ -68,10 +80,43 @@ public class TDBService {
     }
 
   }
-
-  public void loadData(String graphUri, Model model) {
+  public void insertModel(String graphUri, Model model) {
     var triples = ModelUtils.toString(model, RDFLanguages.NTRIPLES);
     String updateQuery = String.format("INSERT DATA { GRAPH <%s> { %s } }", graphUri, triples);
     executeUpdateQuery(updateQuery);
+  }
+
+  public void batchLoadData(String graph, Model model) {
+    log.debug("running import triples with batch size {}, model size: {}, graph: <{}>", batchSize, model.size(), graph);
+    List<Triple> triples = model.getGraph().find().toList(); //duplicate so we can splice
+    Lists.partition(triples, batchSize)
+         .stream()
+         .parallel()
+         .map(batch -> {
+           Model batchModel = ModelFactory.createDefaultModel();
+           Graph batchGraph = batchModel.getGraph();
+           batch.forEach(batchGraph::add);
+           return batchModel;
+         })
+         .forEach(batchModel -> this.insertModelOrRetry (graph, batchModel));
+  }
+
+  private void insertModelOrRetry(String graph, Model batchModel) {
+    int retryCount = 0;
+    boolean success = false;
+    do {
+      try {
+        this.insertModel(graph, batchModel);
+        success = true;
+        break;
+      }
+      catch (Exception e) {
+        log.error("an error occurred, retry count {}, max retry {}, error: {}", retryCount, maxRetry, e);
+        retryCount += 1;
+      }
+    } while (retryCount < maxRetry);
+    if (!success) {
+      throw new RuntimeException("Reaching max retries. Check the logs for further details.");
+    }
   }
 }
