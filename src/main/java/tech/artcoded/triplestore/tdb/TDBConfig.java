6413a2dbe6548ed34b0c7ad85284b64526d32cfd
Nordine Bittich <nordine@bittich.be>
2021-12-23 20:14:52 +0100

    assembler model

--- src/main/java/tech/artcoded/triplestore/tdb/TDBConfig.java
+++ src/main/java/tech/artcoded/triplestore/tdb/TDBConfig.java
@@ -1,8 +1,19 @@
 package tech.artcoded.triplestore.tdb;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.dboe.base.file.Location;
 import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.AnonId;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.sparql.core.assembler.AssemblerUtils;
+import org.apache.jena.tdb2.TDB2;
 import org.apache.jena.tdb2.TDB2Factory;
+import org.apache.jena.tdb2.assembler.VocabTDB2;
+import org.apache.jena.tdb2.params.StoreParams;
+import org.apache.jena.tdb2.store.TDB2StorageBuilder;
+import org.apache.jena.vocabulary.RDF;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +25,23 @@ import java.io.File;
 public class TDBConfig {
   @Value("${triplestore.database.dir}")
   private String tripleStoreDir;
+  @Value("${triplestore.database.unionDefaultGraph}")
+  private boolean unionDefaultGraph;
 
   @Bean(destroyMethod = "close")
   public Dataset database() {
     File dir = new File(tripleStoreDir);
     if (!dir.exists()) {
-      log.info("creating folder {}: {}", tripleStoreDir, dir.mkdirs());
+      log.info("creating directory {}: {}", tripleStoreDir, dir.mkdirs());
     }
-    return TDB2Factory.connectDataset(tripleStoreDir);
+
+    Model assemblerModel = ModelFactory.createDefaultModel();
+    Resource dataset = assemblerModel.createResource(AnonId.create("dataset"));
+    dataset.addProperty(RDF.type,VocabTDB2.tDatasetTDB);
+    dataset.addProperty(VocabTDB2.pLocation, tripleStoreDir);
+    dataset.addLiteral(VocabTDB2.pUnionDefaultGraph, unionDefaultGraph);
+
+    return (Dataset)AssemblerUtils.build(assemblerModel, VocabTDB2.tDatasetTDB);
+
   }
 }
