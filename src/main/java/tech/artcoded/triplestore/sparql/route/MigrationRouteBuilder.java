74b19085abaed53cd0dc44a58c2cd473912adaa5
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2021-12-23 17:06:49 +0100

    migrations capabilities (#7)
    * migrations capabilities
    
    * bugfix & more logging

---
+++ src/main/java/tech/artcoded/triplestore/sparql/route/MigrationRouteBuilder.java
@@ -0,0 +1,96 @@
+package tech.artcoded.triplestore.sparql.route;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.apache.camel.Body;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Header;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.compress.utils.FileNameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFLanguages;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import tech.artcoded.triplestore.sparql.ModelUtils;
+import tech.artcoded.triplestore.tdb.TDBService;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.compress.utils.FileNameUtils.getBaseName;
+
+@Component
+public class MigrationRouteBuilder extends RouteBuilder {
+  public static final String HEADER_TITLE = "NotificationTitle";
+  public static final String HEADER_TYPE = "NotificationType";
+  public static final String SYNC_FILE_TRIPLESTORE = "SYNC_FILE_TRIPLESTORE";
+  public static final String CORRELATION_ID = "CorrelationId";
+  public static final String NOTIFICATION_ENDPOINT = "jms:topic:notification";
+  private final TDBService tdbService;
+
+  @Value("${triplestore.migration.defaultGraph}")
+  private String defaultGraph;
+
+  private static final Cache<String, String> GRAPH_CACHE = Caffeine.newBuilder()
+                                                           .expireAfterAccess(Duration.ofMinutes(5))
+                                                           .maximumSize(1000)
+                                                           .build();
+
+  public MigrationRouteBuilder(TDBService tdbService) {
+    this.tdbService = tdbService;
+  }
+
+  @Override
+  public void configure() throws Exception {
+    onException(Exception.class)
+            .handled(true)
+            .log("Exception occurred due: ${exception.message}")
+    ;
+
+    from("file:{{triplestore.migration.dir}}?sortBy=file:name;file:modified")
+            .routeId("MigrationRoute::Entrypoint")
+            .log("receiving file '${headers.%s}', will execute migration to the triplestore".formatted(Exchange.FILE_NAME))
+            .convertBodyTo(byte[].class)
+            .choice()
+              .when(header(Exchange.FILE_NAME).endsWith("graph"))
+                  .bean(()-> this, "addGraphToCache")
+              .otherwise()
+                  .setProperty(HEADER_TITLE, simple("'${headers.%s}', has been executed to the triplestore".formatted(Exchange.FILE_NAME)))
+                  .setProperty(HEADER_TYPE, constant(SYNC_FILE_TRIPLESTORE))
+                  .bean(() -> this, "performMigration")
+                  .setHeader(CORRELATION_ID, body())
+                  .setHeader(HEADER_TITLE, exchangeProperty(HEADER_TITLE))
+                  .setHeader(HEADER_TYPE, exchangeProperty(HEADER_TYPE))
+                  .to(ExchangePattern.InOnly, NOTIFICATION_ENDPOINT)
+              .endChoice();
+  }
+
+  void addGraphToCache(@Body byte[] file,
+                       @Header(Exchange.FILE_NAME) String fileName){
+    GRAPH_CACHE.put(getBaseName(fileName), IOUtils.toString(file, StandardCharsets.UTF_8.name()));
+  }
+
+  String performMigration(@Body byte[] file,
+                          @Header(Exchange.FILE_NAME) String fileName) {
+    String extension = FileNameUtils.getExtension(fileName);
+
+    if("sparql".equalsIgnoreCase(extension)){
+      tdbService.executeUpdateQuery(IOUtils.toString(file, StandardCharsets.UTF_8.name()));
+       return UUID.randomUUID().toString();
+    }
+
+    Lang lang = RDFLanguages.filenameToLang(fileName);
+    Model model = ModelUtils.toModel(new ByteArrayInputStream(file), lang);
+    String graph = ofNullable(GRAPH_CACHE.getIfPresent(getBaseName(fileName))).orElseGet(() -> defaultGraph);
+    tdbService.batchLoadData(graph, model);
+    return UUID.randomUUID().toString();
+
+  }
+}
