df6bff43aa3cee2023c660f85e2eeb4847c969ca
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2022-01-01 12:58:36 +0100

    StreamingResponseBody for large response (#13)
    * StreamingResponseBody for large response
    
    * refactoring

--- src/main/java/tech/artcoded/triplestore/sparql/QueryParserUtil.java
+++ src/main/java/tech/artcoded/triplestore/sparql/QueryParserUtil.java
@@ -1,38 +1,54 @@
 package tech.artcoded.triplestore.sparql;
 
 
-import org.apache.jena.query.Query;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.query.QueryException;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.update.UpdateFactory;
-import org.apache.jena.update.UpdateRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static java.util.Optional.ofNullable;
 
 public interface QueryParserUtil {
   Logger LOGGER = LoggerFactory.getLogger(QueryParserUtil.class);
 
-  static Object parseOperation(String query) {
+  enum QueryType {UPDATE, READ}
+
+  record QueryTuple(Object query, QueryType type) {
+  }
+
+  static Optional<QueryTuple> parseOperation(String query, boolean forceRead) {
+
+    return ofNullable(query).filter(StringUtils::isNotEmpty)
+                            .flatMap(q -> parseQuery(q, forceRead).or(() -> parseUpdate(q)));
+  }
+
+  static Optional<QueryTuple> parseQuery(String query, boolean forceRead) {
     try {
-      return parseQuery(query);
+      return of(new QueryTuple(QueryFactory.create(query), QueryType.READ));
     }
     catch (QueryException exception) {
-      try {
-        return parseUpdate(query);
+      LOGGER.error("unsupported operation:", exception);
+      if (forceRead) {
+        throw new RuntimeException(exception.getMessage());
       }
-      catch (Exception e) {
-        LOGGER.error("unsupported operation:\n", query);
-        throw new UnsupportedOperationException();
-      }
+      return empty();
     }
   }
 
-  static Query parseQuery(String query) {
-    return QueryFactory.create(query);
-  }
-
-  static UpdateRequest parseUpdate(String query) {
-    return UpdateFactory.create(query);
+  static Optional<QueryTuple> parseUpdate(String query) {
+    try {
+      return of(new QueryTuple(UpdateFactory.create(query), QueryType.UPDATE));
+    }
+    catch (Exception exc) {
+      LOGGER.error("unsupported operation:", exc);
+      throw new RuntimeException(exc.getMessage());
+    }
   }
 
 }
--- src/main/java/tech/artcoded/triplestore/sparql/SparqlEndpoint.java
+++ src/main/java/tech/artcoded/triplestore/sparql/SparqlEndpoint.java
@@ -4,6 +4,8 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.ProducerTemplate;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.query.Query;
 import org.apache.jena.update.UpdateRequest;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,16 +19,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import tech.artcoded.triplestore.tdb.TDBService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
-import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import static java.util.concurrent.CompletableFuture.runAsync;
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static tech.artcoded.triplestore.sparql.QueryParserUtil.parseOperation;
 
 
 @RestController
@@ -49,65 +57,70 @@ public class SparqlEndpoint {
 
   @RequestMapping(value = "/public/sparql",
                   method = {RequestMethod.GET, RequestMethod.POST})
-  public ResponseEntity<String> executePublicQuery(@RequestParam(value = "query",
-                                                                 required = false) String query,
-                                                   @RequestParam(value = "update",
-                                                                 required = false) String update,
-                                                   HttpServletRequest request) {
-    return executeQuery(query, update, request);
+  public ResponseEntity<StreamingResponseBody> executePublicQuery(@RequestParam(value = "query",
+                                                                                required = false) String query,
+                                                                  @RequestParam(value = "update",
+                                                                                required = false) String update,
+                                                                  HttpServletRequest request) {
+    return execute(query, update, request, true);
   }
 
   @RequestMapping(value = "/sparql",
                   method = {RequestMethod.GET, RequestMethod.POST})
-  public ResponseEntity<String> executeQuery(@RequestParam(value = "query",
-                                                           required = false) String query,
-                                             @RequestParam(value = "update",
-                                                           required = false) String update,
-                                             HttpServletRequest request) {
+  public ResponseEntity<StreamingResponseBody> executeQuery(@RequestParam(value = "query",
+                                                                          required = false) String query,
+                                                            @RequestParam(value = "update",
+                                                                          required = false) String update,
+                                                            HttpServletRequest request) {
+    return execute(query, update, request, false);
+  }
+
+  ResponseEntity<StreamingResponseBody> execute(String query, String update, HttpServletRequest request, boolean forceRead) {
     String accept = request.getHeader(ACCEPT);
 
-    if (isNotEmpty(query)) {
-      return tryParseExecute(query, accept);
-    }
-    else {
-      return tryParseExecute(update, accept);
-    }
+    return ofNullable(query).filter(StringUtils::isNotEmpty)
+                            .or(() -> ofNullable(update))
+                            .map(q -> tryParseExecute(q, accept, forceRead))
+                            .orElseGet(ResponseEntity.noContent()::build);
   }
 
-  ResponseEntity<String> tryParseExecute(String query, String accept) {
+
+  ResponseEntity<StreamingResponseBody> tryParseExecute(String query, String accept, boolean forceRead) {
     try {
-      if (isNotEmpty(query)) {
-        var operation = QueryParserUtil.parseOperation(query);
-        if (operation instanceof Query) {
-          return executeRead(query, accept);
-        }
-        else if (operation instanceof UpdateRequest) {
-          if (!canUpdate()) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You cannot perform this action");
-          }
-          return executeUpdate(query);
-        }
-      }
-      return ResponseEntity.ok().build(); // just a ping
+      return parseOperation(query, forceRead).flatMap(operation -> switch (operation.type()) {
+        case READ:
+          if (operation.query() instanceof Query q) yield of(executeRead(q, accept));
+          else yield empty();
+        case UPDATE:
+          if (operation.query() instanceof UpdateRequest q) yield of(executeUpdate(q));
+          else yield empty();
+      }).orElseGet(() -> ResponseEntity.noContent().build());
     }
     catch (Exception exc) {
-      return ResponseEntity.status(400).body("error: %s".formatted(exc.getMessage()));
+      return ResponseEntity.status(400).body((out) -> IOUtils.write("{error: '%s'}".formatted(exc.getMessage()), out, UTF_8));
     }
   }
 
-  ResponseEntity<String> executeRead(String query, String accept) {
-    this.producerTemplate.sendBodyAndHeader("jms:queue:sparql-read", ExchangePattern.InOnly, query,
-                                            "accept", accept);
+  ResponseEntity<StreamingResponseBody> executeRead(Query query, String accept) {
+    runAsync(() -> this.producerTemplate.sendBodyAndHeader("jms:queue:sparql-read", ExchangePattern.InOnly, query.serialize(),
+                                                           "accept", accept));
     var response = tdbService.executeQuery(query, accept);
     return ResponseEntity.status(200).header(CONTENT_TYPE, response.getContentType())
-                         .body(response.getBody());
+                         .body((out) -> {
+                           try (var is = response.getBody()) {
+                             IOUtils.copyLarge(is, out);
+                           }
+                         });
   }
 
-  ResponseEntity<String> executeUpdate(String update) {
-
-    this.producerTemplate.sendBody("jms:queue:sparql-update", ExchangePattern.InOnly, update);
+  ResponseEntity<StreamingResponseBody> executeUpdate(UpdateRequest update) {
+    if (!canUpdate()) {
+      return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                           .body(out -> IOUtils.write("You cannot perform this action", out, UTF_8));
+    }
+    CompletableFuture.runAsync(() -> this.producerTemplate.sendBody("jms:queue:sparql-update", ExchangePattern.InOnly, update.toString()));
     return ResponseEntity.status(200)
-                         .body("processing update");
+                         .body((out) -> IOUtils.write("processing update", out, UTF_8));
   }
 
   boolean canUpdate() {
